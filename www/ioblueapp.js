d1762b6b8644b26eb9fe70ba1a81e18b6bfc4eee
Kushal Kumar <kushal@vensi.com>
2016-07-08 17:05:29 +0530

    rename

--- www/ioblueapp.js
+++ www/ioblueapp.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.request = function(action, message, success, error) {
-    exec(successCallback, errorCallback, "io.blueapp", action, [message]);
+    exec(successCallback, errorCallback, "IOBlueApp", action, [message]);
 };
