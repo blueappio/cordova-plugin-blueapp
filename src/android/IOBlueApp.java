319c4246896ef34900e9dd5ce78fa6acd29c77fe
Kushal Kumar <kushal@vensi.com>
2016-08-03 20:05:28 +0530

    Cleaned the Android cordova plugin.

--- src/android/IOBlueApp.java
+++ src/android/IOBlueApp.java
@@ -1,12 +1,12 @@
 package com.vensi.plugin;
 
+import android.content.Context;
 import android.util.Log;
-
-import com.vensi.connect.MainActivity;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
+import org.gatt_ip.GATTIP;
 import org.gatt_ip.GATTIPListener;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,20 +14,35 @@ import org.json.JSONException;
 public class IOBlueApp extends CordovaPlugin {
     public static String TAG = "IOBlueApp";
 
+    private GATTIP gattip;
+
+    /**
+     * Constructor.
+     */
+    public IOBlueApp() {
+        Log.d(TAG, "IOBlueApp()");
+    }
+
     @Override
     public boolean execute(String action, JSONArray data, final CallbackContext callbackContext) throws JSONException {
 
         switch (action) {
             case "init":
-                MainActivity.gattip.setGATTIPListener(new GATTIPListener() {
-                    @Override
-                    public void response(String gattipResponse) {
-                        Log.d(TAG, "IOBlueApp Response  >>>>>>>>>> : " + gattipResponse);
-                        PluginResult result = new PluginResult(PluginResult.Status.OK, gattipResponse);
-                        result.setKeepCallback(true);
-                        callbackContext.sendPluginResult(result);
-                    }
-                });
+                Log.d(TAG, "init()");
+                if(gattip == null) {
+                    Context context = this.cordova.getActivity().getApplicationContext();
+                    gattip = new GATTIP(context);
+
+                    gattip.setGATTIPListener(new GATTIPListener() {
+                        @Override
+                        public void response(String gattipResponse) {
+                            Log.d(TAG, "IOBlueApp Response  >>>>>>>>>> : " + gattipResponse);
+                            PluginResult result = new PluginResult(PluginResult.Status.OK, gattipResponse);
+                            result.setKeepCallback(true);
+                            callbackContext.sendPluginResult(result);
+                        }
+                    });
+                }
                 return true;
             case "message":
                 String gattipRequest = data.getString(0);
@@ -35,11 +50,15 @@ public class IOBlueApp extends CordovaPlugin {
                 if (gattipRequest != null) {
                     try {
                         Log.d(TAG, "IOBlueApp Request <<<<<<<<<<< : " + gattipRequest);                        
-                        MainActivity.gattip.request(gattipRequest);
+                        gattip.request(gattipRequest);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
+                return true;
+            case "close":
+                Log.d(TAG, "close()");
+                gattip = null;
                 return true;
             default:
                 Log.w(TAG, "Invalid ACTION : " + action);            
